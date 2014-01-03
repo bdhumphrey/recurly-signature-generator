f1e06a6eb8b733c9313506ca62cd5b27dd16757f
Chris Eigner <eignerchris@gmail.com>
2014-01-03 15:50:15 -0800

    fixed unicorn config and added Procfile for Heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,14 +1,6 @@
 # Use at least one worker per core if you're on a dedicated server,
 # more will usually help for _short_ waits on databases/caches.
-worker_processes development ? 2 : 6
+worker_processes 6
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
-
-# feel free to point this anywhere accessible on the filesystem
-pid pid_path
-
-# combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
-# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
-# preload
-preload_app true
